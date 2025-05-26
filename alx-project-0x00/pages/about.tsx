106d4b5fee624539e9e5dea9c774e853acc88c58
Ndata <wisdomndata@gmail.com>
2025-05-26 17:26:16 +0100

    Basic Routing

---
+++ alx-project-0x00/pages/about.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const About: React.FC =  () => {
+  return (
+    <div>
+   <h1 className="text-xl font-extralight">About Page</h1>
+    </div>
+  )
+}
+export default About;
